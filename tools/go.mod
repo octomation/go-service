8d2848948c5886ec632ade7db5d0509b18c5a3d7
Kamil Samigullin <kamil@samigullin.info>
2020-11-09 16:27:48 +0000

    update golangci-lint and grpc-gateway; replace github.com/golang/protobuf by google.golang.org/protobuf

--- tools/go.mod
+++ tools/go.mod
@@ -6,13 +6,14 @@ require (
 	github.com/go-swagger/go-swagger v0.25.0
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/mock v1.4.4
-	github.com/golang/protobuf v1.4.3
-	github.com/golangci/golangci-lint v1.34.0
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	github.com/golangci/golangci-lint v1.34.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.1.0
 	github.com/kyoh86/looppointer v0.1.7
 	github.com/twitchtv/twirp v7.1.0+incompatible
 	golang.org/x/exp v0.0.0-20201229011636-eab1b5eb1a03
 	golang.org/x/tools v0.5.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.1
+	google.golang.org/protobuf v1.25.0
 )
 
 replace golang.org/x/tools => github.com/kamilsk/go-tools v0.0.5
--- tools/tools.go
+++ tools/tools.go
@@ -8,22 +8,26 @@ import (
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/protoc-gen-gofast"
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "github.com/kyoh86/looppointer/cmd/looppointer"
 	_ "github.com/twitchtv/twirp/protoc-gen-twirp"
 	_ "golang.org/x/exp/cmd/gorelease"
 	_ "golang.org/x/tools/cmd/goimports"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
 
 //go:generate go install github.com/go-swagger/go-swagger/cmd/swagger
 //go:generate go install github.com/gogo/protobuf/protoc-gen-gofast
 //go:generate go install github.com/golang/mock/mockgen
-//go:generate go install github.com/golang/protobuf/protoc-gen-go
 //go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint
-//go:generate go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+//go:generate go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway
+//go:generate go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2
 //go:generate go install github.com/kyoh86/looppointer/cmd/looppointer
 //go:generate go install github.com/twitchtv/twirp/protoc-gen-twirp
 //go:generate go install golang.org/x/exp/cmd/gorelease
 //go:generate go install golang.org/x/tools/cmd/goimports
+//go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
